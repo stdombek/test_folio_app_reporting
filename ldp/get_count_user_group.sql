8f6da1e3444fde6adcceaab8b22921c98d68f86d
Stefan Dombek <dombek@ub.uni-leipzig.de>
2024-05-07 11:33:51 +0200

    Update get_count_user_group.sql

--- ldp/get_count_user_group.sql
+++ ldp/get_count_user_group.sql
@@ -3,51 +3,29 @@
 DROP FUNCTION IF EXISTS get_count_user_group;
 
 CREATE FUNCTION get_count_user_group(
-
-    /* Parameters */
-
     param_user_group TEXT DEFAULT ''
-
 )
-/* Set return type */
 RETURNS TABLE (
-
-    /* Output table */
-
     group_id          TEXT,
     group_description TEXT,
     group_name        TEXT,
-    count_by_group    INTEGER
-    
+    count_by_group    INTEGER    
 )
 AS 
-/* Code block */
 $$
-/* PROCEDURE section */
-BEGIN 
-
-    /* RETURN value is the result of a query */
-    RETURN QUERY
-
-        /* Query Start */
-        SELECT  
-            user_users.patron_group :: TEXT,
-            user_groups.desc,
-            user_groups.group,
-            COUNT(user_users.id) :: INTEGER
-        FROM
-            public.user_users  
-            LEFT JOIN public.user_groups ON user_groups.id = user_users.patron_group
-        WHERE 
-            ((user_groups.group = param_user_group) OR (param_user_group = ''))
-        GROUP BY 
-            user_users.patron_group,
-            user_groups.desc,
-            user_groups.GROUP
-        ;
-        /* Query End */
-END;
+SELECT  
+    user_users.patron_group :: TEXT,
+    user_groups.desc,
+    user_groups.group,
+    COUNT(user_users.id) :: INTEGER
+FROM
+    public.user_users  
+    LEFT JOIN public.user_groups ON user_groups.id = user_users.patron_group
+WHERE 
+    ((user_groups.group = param_user_group) OR (param_user_group = ''))
+GROUP BY 
+    user_users.patron_group,
+    user_groups.desc,
+    user_groups.GROUP
 $$
-LANGUAGE plpgsql
-STABLE
-PARALLEL SAFE;
+LANGUAGE SQL;
