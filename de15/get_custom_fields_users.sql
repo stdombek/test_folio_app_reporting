e7168ebb05adadfea07c42d0c8fe8ea94efc3e6d
Stefan Dombek <dombek@ub.uni-leipzig.de>
2026-09-15 09:44:18 +0200

    Update get_custom_fields_users.sql

--- de15/get_custom_fields_users.sql
+++ de15/get_custom_fields_users.sql
@@ -84,7 +84,7 @@ SELECT
     string_agg(refid_mehrfachauswahl.option_value, ', ')                :: TEXT,
     jsonb_extract_path_text(users.jsonb, 'customFields', 'textfeld')    :: TEXT,
     jsonb_extract_path_text(users.jsonb, 'customFields', 'textbereich') :: TEXT,
-    jsonb_extract_path_text(users.jsonb)                                :: TEXT
+    users.jsonb                                                         :: TEXT
 FROM 
     folio_de15_users.users
     LEFT JOIN refid_radiobutton ON refid_radiobutton.option_id = jsonb_extract_path_text(users.jsonb, 'customFields', 'radiobutton')
@@ -100,7 +100,8 @@ GROUP BY
     refid_radiobutton.option_value,
     refid_einzelauswahl.option_value,
     jsonb_extract_path_text(users.jsonb, 'customFields', 'textfeld'),
-    jsonb_extract_path_text(users.jsonb, 'customFields', 'textbereich')
+    jsonb_extract_path_text(users.jsonb, 'customFields', 'textbereich'),
+    users.jsonb
 /*
  * MAIN QUERY ENDE
  */
